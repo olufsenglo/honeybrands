ebfe7255c5db2b26983c36cb7406391e0d26f32a
olufsen <honeydrop@gmail.com>
2023-10-11 07:45:55 +0100

    started working on mobile menu

--- src/components/common/home/Header.jsx
+++ src/components/common/home/Header.jsx
@@ -2,10 +2,18 @@ import React, { useState } from "react"
 import logo from "../assets/images/logo.svg"
 import cartimg from "../assets/images/cart.png"
 import { BiSearch } from "react-icons/bi"
+import { BsBagCheck } from "react-icons/bs"
+import { RiUser3Line } from "react-icons/ri"
+import { AiOutlineHeart, AiOutlineMenu, AiOutlineClose, AiOutlineDelete } from "react-icons/ai"
 
 export const Header = () => {
   // navbar
-  
+  const [mobile, setMobile] = useState(false)
+  // scroll navbar
+  window.addEventListener("scroll", function () {
+    const header = this.document.querySelector(".header")
+    header.classList.toggle("active", this.window.scrollY > 100)
+  })  
   return (
     <>
       <header className='header'>
@@ -27,8 +35,7 @@ export const Header = () => {
               </ul>
             </div>
           </nav>
-          <div className='right'>
-            
+          <div className='right'>    
             <div className='right_search'>
               <input type='text' placeholder='Search Products...' />
               <BiSearch className='serachIcon heIcon' />
@@ -37,7 +44,12 @@ export const Header = () => {
               <RiUser3Line className='userIcon heIcon' />
               <AiOutlineHeart className='userIcon heIcon' />
             </div>
-            
+            <div className='right_card'>
+              <button className='button'>
+                <BsBagCheck className='shop heIcon' />
+                MY CART<span> ({getdata.length})</span>
+              </button>
+              </div>
           </div>
         </div>
       </header>
