f97a5fdfa6aab903e260bb72f92f856c9fb17e59
olufsen <honeydrop@gmail.com>
2023-10-11 07:36:31 +0100

    added nav data

--- src/components/common/home/Header.jsx
+++ src/components/common/home/Header.jsx
@@ -2,14 +2,6 @@ import React, { useState } from "react"
 import logo from "../assets/images/logo.svg"
 import cartimg from "../assets/images/cart.png"
 import { BiSearch } from "react-icons/bi"
-import { BsBagCheck } from "react-icons/bs"
-import { RiUser3Line } from "react-icons/ri"
-import { AiOutlineHeart, AiOutlineMenu, AiOutlineClose, AiOutlineDelete } from "react-icons/ai"
-import { navlist } from "../assets/data/data"
-import { connect, useDispatch, useSelector } from "react-redux"
-import { Link } from "react-router-dom"
-import { DELETE } from "../../controller/action"
-import { useEffect } from "react"
 
 export const Header = () => {
   // navbar
