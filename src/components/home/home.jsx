91c7acc658ed3209292a30fb93e362a1bb9ec2c4
olufsen <honeydrop@gmail.com>
2023-10-11 08:08:45 +0100

    working on price component

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,6 +3,7 @@ import { Banner } from "./banner/Banner"
 import { Card } from "./Hero/Card"
 import { Hero } from "./Hero/Hero"
 import { Product } from "./product/Product"
+import { TopProduct } from "./top/TopProduct"
 
 export const Home = () => {
   return (
@@ -11,6 +12,7 @@ export const Home = () => {
       <Card />
       <Product />
       <Banner />
+      <TopProduct />
 
     </>
   )
