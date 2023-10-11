e3754c53dde4d5c9436d2dfe25b9574e38e47a49
olufsen <honeydrop@gmail.com>
2023-10-11 07:32:26 +0100

    fixed logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Pages } from '../../Gosto-Template/src/components/pages/Pages';
+import "./style/main.scss"
 
 function App() {
     return ( 
--- src/components/common/home/Header.jsx
+++ src/components/common/home/Header.jsx
@@ -1,9 +1,48 @@
-import React from "react"
+import React, { useState } from "react"
+import logo from "../assets/images/logo.svg"
+import cartimg from "../assets/images/cart.png"
+import { BiSearch } from "react-icons/bi"
+import { BsBagCheck } from "react-icons/bs"
+import { RiUser3Line } from "react-icons/ri"
+import { AiOutlineHeart, AiOutlineMenu, AiOutlineClose, AiOutlineDelete } from "react-icons/ai"
+import { navlist } from "../assets/data/data"
+import { connect, useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+import { DELETE } from "../../controller/action"
+import { useEffect } from "react"
 
 export const Header = () => {
+  // navbar
+  
   return (
     <>
-      Header
+      <header className='header'>
+        <div className='container'>
+          <nav>
+            
+            <div className='left'>
+              <Link to='/'>
+                <img src={logo} alt='logo' />
+              </Link>
+            </div>
+            <div className='center'>
+              <ul className={mobile ? "mobile-nav" : "menu"}>
+                {navlist.map((nav, i) => (
+                  <li key={i}>
+                    <Link to={nav.path}>{nav.text}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </nav>
+          <div className='right'>
+            
+            
+            
+          </div>
+        </div>
+      </header>
     </>
   )
 }
+
