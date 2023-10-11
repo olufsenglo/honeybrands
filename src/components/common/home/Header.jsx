bf48bcf8681a9689e9435684a772664d10b8ab60
olufsen <honeydrop@gmail.com>
2023-10-11 08:24:38 +0100

    finished cart on header component

--- src/components/common/home/Header.jsx
+++ src/components/common/home/Header.jsx
@@ -5,26 +5,55 @@ import { BiSearch } from "react-icons/bi"
 import { BsBagCheck } from "react-icons/bs"
 import { RiUser3Line } from "react-icons/ri"
 import { AiOutlineHeart, AiOutlineMenu, AiOutlineClose, AiOutlineDelete } from "react-icons/ai"
+import { navlist } from "../assets/data/data"
 import { connect, useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+import { DELETE } from "../../controller/action"
+import { useEffect } from "react"
 
 export const Header = () => {
   // navbar
   const [mobile, setMobile] = useState(false)
-  // scroll navbar
+  // cartopen and close
   const [cartList, setCartList] = useState(false)
   const handleClose = () => {
     setCartList(null)
   }
-
+  // scroll navbar
   window.addEventListener("scroll", function () {
     const header = this.document.querySelector(".header")
     header.classList.toggle("active", this.window.scrollY > 100)
-  }) 
-  
+  })
+
   // cart add in shop
   const getdata = useSelector((state) => state.cartReducer.carts)
   console.log(getdata)
-  
+
+  // delete cart
+  const dispatch = useDispatch()
+  const delet = (id) => {
+    dispatch(DELETE(id))
+  }
+
+  // total prcie
+  const [price, setPrice] = useState(0)
+  console.log(price)
+
+  const totals = () => {
+    let price = 0
+    getdata.map((e, i) => {
+      price = parseFloat(e.price) * e.qty + price
+    })
+    setPrice(price)
+  }
+
+  useEffect(() => {
+    totals()
+  }, [totals])
+
+  const handleCloses = () => {
+    setCartList(null)
+  }
   return (
     <>
       <header className='header'>
@@ -32,7 +61,7 @@ export const Header = () => {
           <nav>
             <div className='toggle'>
               <button onClick={() => setMobile(!mobile)}>{mobile ? <AiOutlineClose className='close heIcon' /> : <AiOutlineMenu className='open heIcon' />}</button>
-            </div>            
+            </div>
             <div className='left'>
               <Link to='/'>
                 <img src={logo} alt='logo' />
@@ -48,7 +77,7 @@ export const Header = () => {
               </ul>
             </div>
           </nav>
-          <div className='right'>    
+          <div className='right'>
             <div className='right_search'>
               <input type='text' placeholder='Search Products...' />
               <BiSearch className='serachIcon heIcon' />
@@ -58,13 +87,50 @@ export const Header = () => {
               <AiOutlineHeart className='userIcon heIcon' />
             </div>
             <div className='right_card'>
-              <button className='button'>
+              <button className='button' onClick={() => setCartList(!cartList)}>
                 <BsBagCheck className='shop heIcon' />
                 MY CART<span> ({getdata.length})</span>
               </button>
               <div className={cartList ? "showCart" : "hideCart"}>
+                {getdata.length ? (
+                  <section className='details'>
+                    <div className='details_title'>
+                      <h3>Photo</h3>
+                      <p>Product Name</p>
+                    </div>
+                    {getdata.map((e) => (
+                      <div className='details_content'>
+                        <div className='details_content_img'>
+                          <Link to={`/cart/${e.id}`} onClick={handleCloses}>
+                            <img src={e.cover} alt='' />
+                          </Link>
+                        </div>
+                        <div className='details_content_detail'>
+                          <div className='details_content_detail_price'>
+                            <p>{e.title.slice(0, 20)}...</p>
+                            <p>Price : ${e.price}</p>
+                            <p>Quantity : {e.qty}</p>
+                          </div>
+                        </div>
+                        <div className='details_content_detail_icon'>
+                          <i onClick={() => delet(e.id)}>
+                            <AiOutlineDelete />
+                          </i>
+                        </div>
+                      </div>
+                    ))}
+                    <div className='details_total'>
+                      <h4>Total : ${price}</h4>
+                    </div>
+                  </section>
+                ) : (
+                  <div className='empty'>
+                    <p>Your cart is empty</p>
+                    <img src={cartimg} alt='' />
+                  </div>
+                )}
               </div>
-              </div>
+            </div>
           </div>
         </div>
       </header>
