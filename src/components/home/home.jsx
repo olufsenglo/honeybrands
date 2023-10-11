637f992480aafaaf9d59b9837b13bf5d5ce8b6f7
olufsen <honeydrop@gmail.com>
2023-10-11 08:12:13 +0100

    working on blog, but for home page for now

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Banner } from "./banner/Banner"
+import { Blog } from "./blog/Blog"
 import { Card } from "./Hero/Card"
 import { Hero } from "./Hero/Hero"
 import { Price } from "./price/Price"
@@ -17,6 +18,7 @@ export const Home = () => {
       <TopProduct />
       <Price />
       <Testimonial />
+      <Blog />
     </>
   )
 }
